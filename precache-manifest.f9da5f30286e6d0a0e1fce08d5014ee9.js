ab36ea89f8b9295710ca548d4435ca2c9f8929da
Rodion <ovlavatskaya@gmail.com>
2022-03-25 22:37:06 +0700

    Updates

---
+++ precache-manifest.f9da5f30286e6d0a0e1fce08d5014ee9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "02817a55947f47356cd1",
+    "url": "./static/css/main.2af25bad.chunk.css"
+  },
+  {
+    "revision": "02817a55947f47356cd1",
+    "url": "./static/js/main.02817a55.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "f6ca3cbe0c8a4c9d808c",
+    "url": "./static/css/2.57b60c2e.chunk.css"
+  },
+  {
+    "revision": "f6ca3cbe0c8a4c9d808c",
+    "url": "./static/js/2.f6ca3cbe.chunk.js"
+  },
+  {
+    "revision": "3df961b1477634f02b7a17d0ec6e6680",
+    "url": "./static/media/click.3df961b1.png"
+  },
+  {
+    "revision": "bfb071daa9fe1472dc5466b4c7862ad7",
+    "url": "./index.html"
+  }
+];
